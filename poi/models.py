20b3dc88f456cd7fc889326cf69f0dca1bcf32a0
turker <turker.gurcan@yandex.com.tr>
2023-07-11 14:19:50 +0000

    poi

--- poi/models.py
+++ poi/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.gis.db import models
+
+class Category(models.Model):
+    category_name = models.CharField('Category Name', max_length=50)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = '"poiapp"."categories"'
+    def __str__(self):
+        return self.category_name
+
+class SubCategory(models.Model):
+    categories = models.ForeignKey('Category',on_delete=models.CASCADE)
+    category_name = models.CharField('Category Name', max_length=50)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = '"poiapp"."subcategories"'
+    def __str__(self):
+        return self.category_name
 
 # Create your models here.
