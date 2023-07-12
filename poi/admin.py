de4cba75da09f9752633ba68863bd83a9a6caf36
turker <turker.gurcan@yandex.com.tr>
2023-07-12 06:32:50 +0000

    fixed

--- poi/admin.py
+++ poi/admin.py
@@ -3,5 +3,18 @@ from .models import Category,SubCategory
 
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(SubCategory)
+#admin.site.register(Category)
+#admin.site.register(SubCategory)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=["category_name"]
+    class Meta:
+        model = Category
+
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display=["categories","category_name"]
+    search_fields=["categories"]
+    class Meta:
+        model = SubCategory
